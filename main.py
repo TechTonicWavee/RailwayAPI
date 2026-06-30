b5638521789f31508bd57d9f94f3bdde0ea4cabc
Priyanshu raj <priyanshu84510@gmail.com>
2026-06-30 20:31:07 +0530

    first endpoints

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"message": "Hello, World!"}
+
+@app.get('/about')
+def about():
+    return {"message": "2026 going to be my year."}
